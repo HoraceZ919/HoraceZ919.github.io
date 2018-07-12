5578928ec2e684e21204bb9c6e7327a61b7e51fc
HoraceZ <418238239@qq.com>
2018-07-12 16:18:17 +0800

    。。。

--- miao/lodash/horacez919-lodash.js
+++ miao/lodash/horacez919-lodash.js
@@ -1,11 +1,11 @@
-var HoraceZ919 = {
+var horacez919 = {
   chunk:function(nums, size) {
     var result=[]
     for (var i = 0, len=nums.length; i < len; i+=size) {
       result.push(nums.slice(i, i+size))
     }
     return result
-  }
+  },
 
   compact:function(nums) {
     var result=[]
@@ -15,7 +15,7 @@ var HoraceZ919 = {
       }
     }
     return result
-  }
+  },
 
   drop:function(array, num) {
     var result=[]
@@ -23,7 +23,7 @@ var HoraceZ919 = {
     var len=array.length
     result=array.slice(num, len)
     return result
-  }
+  },
 
   dropRight:function(array, num) {
     var result=[]
@@ -31,7 +31,7 @@ var HoraceZ919 = {
     var len=array.length
     result=array.slice(0, len-num)
     return result
-  }
+  },
 
   fill:function(array, value, start, end) {
     start=start || 0
@@ -39,11 +39,11 @@ var HoraceZ919 = {
     for (var i = 0, len=array.length; i < len; i++) {
       array[i]=value
     }
-  }
+  },
 
   head:function(array) {
     return array=null?null:array[0]
-  }
+  },
 
   indexOf:function(array, value, index) {
     index=index || 0
@@ -60,10 +60,37 @@ var HoraceZ919 = {
           }
         }
       }
-  }
+  },
 
 
   initial:function(array) {
     return array=null?null:array.slice(0, array.length-1)
-  }
+  },
+
+  last:function(array) {
+    return array=null?null:array[array.length-1]
+  },
+
+  lastIndexOf:function(array, value, index) {
+    index=index || array.length-1
+    for (var i = index; i >= 0; i--) {
+      if (array[i] === value) {
+        return i
+      }
+    }
+    return -1
+  },
+
+
+  'prototype.reverse':function(nums, left, right) {
+    var left=left || 0, right=right || nums.length-1
+    if (left < right) {
+      [nums[left], nums[right]]=[nums[right], nums[left]]
+      reverse(nums, left+1, right-1)
+    }
+    return nums
+  },
+
+
+
 }
