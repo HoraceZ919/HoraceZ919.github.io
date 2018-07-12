0aa819ef4c803d9d8084f3d60e39944f79965eb2
HoraceZ <418238239@qq.com>
2018-07-12 10:14:12 +0800

    11

---
+++ miao/lodash/HoraceZ919-lodash.js
@@ -0,0 +1,43 @@
+var HoraceZ919 = {
+  chunk:function(nums, size) {
+    var result=[]
+    for (var i = 0, len=nums.length; i < len; i+=size) {
+      result.push(nums.slice(i, i+size))
+    }
+    return result
+  }
+
+  compact:function(nums) {
+    var result=[]
+    for (var i = 0, len=nums.length; i < len; i++) {
+      if (nums[i]) {
+        result.push(nums[i])
+      }
+    }
+    return result
+  }
+
+  drop:function(array, num) {
+    var result=[]
+    var num=num || 1
+    var len=array.length
+    result=array.slice(num, len)
+    return result
+  }
+
+  dropRight:function(array, num) {
+    var result=[]
+    var num=num || 1
+    var len=array.length
+    result=array.slice(0, len-num)
+    return result
+  }
+
+  fill:function(array, value, start, end) {
+    start=start || 0
+    end=end || array.length
+    for (var i = 0, len=array.length; i < len; i++) {
+      array[i]=value
+    }
+  }
+}
